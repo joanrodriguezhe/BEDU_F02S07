173b416df8fa8369029ae5c9839a0cc0942a8b40
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-27 20:51:08 -0700

    Reto 2

--- main.js
+++ main.js
@@ -34,3 +34,52 @@ for (img of [...imgTags]) {
     let textAlt = document.createTextNode(img.alt);
     img.parentNode.replaceChild(textAlt, img);
 }
+
+//Reto 2
+const data = [
+    { name: "Kilimanjaro", height: 5895, place: "Tanzania" },
+    { name: "Everest", height: 8848, place: "Nepal" },
+    { name: "Mount Fuji", height: 3776, place: "Japan" },
+    { name: "Vaalserberg", height: 323, place: "Netherlands" },
+    { name: "Denali", height: 6168, place: "United States" },
+    { name: "Popocatepetl", height: 5465, place: "Mexico" },
+    { name: "Mont Blanc", height: 4808, place: "Italy/France" }
+];
+
+let div = document.getElementById('mountains');
+div.appendChild(document.createElement('table'));
+
+let table = document.getElementsByTagName('table')[0];
+
+table.appendChild(document.createElement('tr'));
+
+let tr = document.createElement('tr');
+
+Object.keys(data[0]).forEach(text => {
+    let head = document.createTextNode(text);
+    let th = document.createElement('th');
+    th.appendChild(head);
+    tr.appendChild(th);
+})
+table.appendChild(tr);
+tr = document.createElement('tr');
+
+data.forEach(row => {
+    let name = document.createTextNode(row.name);
+    let tdName = document.createElement('td');
+    tdName.appendChild(name)
+    tr.appendChild(tdName);
+    let height = document.createTextNode(row.height);
+    let tdHeight = document.createElement('td');
+    tdHeight.style.textAlign = "right";
+    tdHeight.appendChild(height)
+    tr.appendChild(tdHeight);
+    let place = document.createTextNode(row.place);
+    let tdPlace = document.createElement('td');
+    tdPlace.appendChild(place)
+    tr.appendChild(tdPlace);
+
+    table.appendChild(tr);
+    tr = document.createElement('tr');
+});
+
